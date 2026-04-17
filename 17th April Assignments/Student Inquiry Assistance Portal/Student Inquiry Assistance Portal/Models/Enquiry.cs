f5e99c04a8214b80d43f0bb4b87d27733ddd7977
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2026-04-17 14:20:55 +0530

    17th April assignment uploaded

---
+++ 17th April Assignments/Student Inquiry Assistance Portal/Student Inquiry Assistance Portal/Models/Enquiry.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Student_Inquiry_Assistance_Portal.Models
+{
+    public class Enquiry
+    {
+        [Key]
+        public int EnquiryID { get; set; }
+
+        public DateTime EnquiryDate { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string EnquiryType { get; set; }
+
+        // FK
+        public int StudentId { get; set; }
+        public int CourseID { get; set; }
+
+        [ForeignKey("StudentId")]
+        public Student? Student { get; set; }
+
+        [ForeignKey("CourseID")]
+        public Course? Course { get; set; }
+    }
+}
---
+++ 17th April Assignments/Student Inquiry Assistance Portal/Student Inquiry Assistance Portal/Program.cs
@@ -0,0 +1,53 @@
+
+using Microsoft.EntityFrameworkCore;
+using Student_Inquiry_Assistance_Portal.Data;
+
+namespace Student_Inquiry_Assistance_Portal
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            //addition
+            builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("constring")));
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+         
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
